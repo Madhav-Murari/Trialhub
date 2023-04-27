a897ee840dadbffe4a5cec4e940bdc35ecead91e
Vivek Srivastava <vivek@shuniyavigyan.com>
2023-04-27 11:56:51 +0530

    Employee profile

--- src/components/Manager/TeamCard.jsx
+++ src/components/Manager/TeamCard.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Image from "next/image";
 
 export default function TeamCard(props) {
   return (
     <>
       <div className="flex flex-col items-center m-1 shadow p-1 rounded-md mx-2">
-        <Image
-          src={props.name}
-          className="rounded w-[8rem] md:w-[10rem]"
+        <img
+          src={props.photo}
+          className="rounded w-[8rem] h-[10rem] md:w-[10rem]"
           alt="img"
           priority={false}
         />
-        <div className="rounded">{props.username}</div>
+        <div className="rounded">{props.name}</div>
         <div className="rounded text-[#00a3ff] text-sm">
           {props.designation}
         </div>
