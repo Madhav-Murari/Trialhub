591caa5eb5038242646dc270cd4c2e55901f85f8
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-06-07 01:03:45 +0530

    task page

--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -30,20 +30,18 @@ export default function index({}: Props) {
 
   return (
     <>
-      <div className="bg-light-primary p-4 h-full">
+      <div className="p-4 h-full">
         <div className="flex flex-row md:flex-col">
-          <div className="bg-lighter-grey basis-1/2 flex flex-col md:flex-row items-center justify-between font-semibold p-3 rounded-md">
+          <div className="bg-lighter-grey basis-1/2 flex flex-col md:flex-row items-center justify-between font-medium p-3 rounded-md">
             <div className="text-left w-full md:flex md:w-3/20">User ID</div>
             <div className="text-left w-full md:flex md:w-3/20">Client ID</div>
-            <div className="text-left w-full md:flex md:w-3/20">
-              Task Assigned
-            </div>
+            <div className="text-left w-full md:flex md:w-3/20">Task Assigned</div>
             <div className="text-left w-full md:flex md:w-3/20">Status</div>
             <div className="text-left w-full md:flex md:w-3/20">Start Date</div>
             <div className="text-left w-full md:flex md:w-3/20">End Date</div>
           </div>
 
-          <div className="bg-white basis-1/2 rounded-md mx-1">
+          <div className="bg-white basis-1/2 rounded-md shadow mx-1">
             <TaskList tasks={taskData} />
           </div>
         </div>
