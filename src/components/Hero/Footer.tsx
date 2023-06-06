ae4cd9b6be630cbb80032d8879333f5348a68f39
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-06-06 19:40:16 +0530

    ui updated

--- src/components/Hero/Footer.tsx
+++ src/components/Hero/Footer.tsx
@@ -8,19 +8,19 @@ const Footer: React.FC = () => {
                     <p className="text-gray-400">© 2023 Trialhub. All rights reserved.</p>
                     <div className="flex flex-col justify-center items-center mt-4">
                         <div className='my-2'>
-                            <a href="#" className="text-gray-400 hover:text-white mx-3">Terms of Service</a>
+                            <a href="#" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">Terms of Service</a>
                             <span className="text-gray-400">|</span>
-                            <a href="#" className="text-gray-400 hover:text-white mx-3">Privacy Policy</a>
+                            <a href="#" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">Privacy Policy</a>
                             <span className="text-gray-400">|</span>
-                            <a href="#" className="text-gray-400 hover:text-white mx-3">Contact Us</a>
+                            <a href="#" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">Contact Us</a>
                         </div>
 
                         <div className='my-2'>
-                            <a href="https://www.linkedin.com/company/trialshoppy/" target='_blank' rel="noreferrer" className="text-gray-400 hover:text-white mx-3">LinkedIn</a>
+                            <a href="https://www.linkedin.com/company/trialshoppy/" target='_blank' rel="noreferrer" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">LinkedIn</a>
+                            <span className="text-gray-400 hover:text-[#FF725E]">|</span>
+                            <a href="https://www.instagram.com/trialshoppy/" target='_blank' rel="noreferrer" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">Instagram</a>
                             <span className="text-gray-400">|</span>
-                            <a href="https://www.instagram.com/trialshoppy/" target='_blank' rel="noreferrer" className="text-gray-400 hover:text-white mx-3">Instagram</a>
-                            <span className="text-gray-400">|</span>
-                            <a href="#" className="text-gray-400 hover:text-white mx-3">Twitter</a>
+                            <a href="#" className="text-gray-400 hover:text-white mx-3 hover:text-[#FF725E]">Twitter</a>
                         </div>
                     </div>
                 </div>
